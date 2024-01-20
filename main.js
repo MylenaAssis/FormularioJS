9cf06c1930f13b8520c89b81e2310ad280c822ba
Mylena Assis <lena.assis.dev@gmail.com>
2024-01-19 23:00:56 -0300

    Added - estilizando com JS

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 //seleciona o form pelo id
 const form = document.getElementById("form-deposito")
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+let formEValido = false;
 
 //função validar nome completo
 function ValidaNome(nomeCompleto) {
@@ -11,23 +13,36 @@ function ValidaNome(nomeCompleto) {
 //atributo do elemento que vai fazer isso: nome.addEventListener(evento, função)
 form.addEventListener('submit', function(e) {
     //validaçao do formulario
-    let formEValido = false;
     e.preventDefault();
 
 
-    const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroConta = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente ${nomeBeneficiario.value} - conta ${numeroConta.value}`
+    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente <b>${nomeBeneficiario.value}</b> - conta <b>${numeroConta.value}</b>`
 
     formEValido = ValidaNome(nomeBeneficiario.value);
     if (formEValido) {
-        alert(mensagemSucesso)
+        const containerMensagemSucesso = document.querySelector('.success-message');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block'
         nomeBeneficiario.value = '';
         numeroConta.value = '';
         valorDeposito.value = '';
         
     } else {
-        alert("Digite o nome completo.")
+        nomeBeneficiario.style.border = '1px solid red'
+        document.querySelector('.error-message').style.display = 'block';
+    }
+})
+
+nomeBeneficiario.addEventListener('keyup', function(e) {
+    console.log(e.target.value)
+    formEValido = ValidaNome(e.target.value);
+    if (!formEValido) {
+        nomeBeneficiario.classList.add('error')
+        document.querySelector('.error-message').style.display = 'block';
+    } else {
+        nomeBeneficiario.style = 'none';
+        document.querySelector('.error-message').style.display = '';
     }
 })
